2baa2baf4967703448652e4be89ecbeaf170d458
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-04-25 19:43:40 +0200

    De welcome page verwijderd standaard van laravel om zo te verplichten om naar de dashboard page te gaan

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
