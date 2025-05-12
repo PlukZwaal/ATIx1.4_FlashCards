5ed610506bbfadb151c7a93c57630b6c0193ae79
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-05-12 22:50:48 +0200

    De call van alle gebruikers verbeterd voor tabel

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
+use App\Models\Deck;
+use App\Models\Card;
+use App\Models\GameSession; 
+use App\Models\User;
 
 class ApiController extends Controller
 {
     public function getAllUsers(): JsonResponse
     {
-        return response()->json(DB::table('users')->get());
+        $users = User::all();
+        foreach ($users as $user) {
+            $decksCount = Deck::where('user_id', $user->id)->count();
+            $cardsCount = 0;
+            $decks = Deck::where('user_id', $user->id)->get();
+            foreach ($decks as $deck) {
+                $cardsCount += Card::where('deck_id', $deck->id)->count();
+            }
+            $sessionsCount = GameSession::where('user_id', $user->id)->count();
+            $user->decks_count = $decksCount;
+            $user->cards_count = $cardsCount;
+            $user->sessions_count = $sessionsCount;
+        }
+        return response()->json($users);
     }
 
     public function getAllDecks(): JsonResponse
